34e59e4376e24cef150de1ae784af46c73cfb5a5
Saad-1719 <chsaad1407@gmail.com>
2025-04-23 08:07:16 +0500

    Add user model with fields for email, full name, hashed password, and timestamps

---
+++ app/models/user_model.py
@@ -0,0 +1,15 @@
+import uuid
+from sqlalchemy import Column, Integer, String, Boolean, DateTime,UUID
+from sqlalchemy.sql import func
+from sqlalchemy.dialects.postgresql import UUID
+from app.services.db import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    email = Column(String, unique=True, index=True, nullable=False)
+    full_name = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
