159a4e5c6c34fceb60b95bc8e0312bd6e9f531cc
Saad-1719 <chsaad1407@gmail.com>
2025-04-27 19:53:37 +0500

    Add refresh token fields and improve token validation
    Introduced `refresh_token` and `refresh_token_expires_at` fields to the `User` model. Refactored `get_current_user` to handle access token type validation and utilize `UserPublic` schema for response. Improved error handling and messaging for invalid tokens.

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -1,9 +1,8 @@
-#get current user from token\
 from fastapi import Depends,HTTPException,status
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordBearer
-from app.core.security import decode_token
-from app.schemas.auth import TokenData
+from app.core.security import decode_access_token
+from app.schemas.auth import UserPublic
 from app.models import user as user_model
 from app.dependencies.database import get_session
 
@@ -11,13 +10,19 @@ oauth2_schema=OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 def get_current_user(token: str = Depends(oauth2_schema),db:Session=Depends(get_session)) -> user_model.User:
     try:
-        token_data = decode_token(token)
-        if not token_data.email:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid Token")
-        user=db.query(user_model.User).filter(user_model.User.email == token_data.email, user_model.User.is_active==True).first()
-        if not user:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="User not found")
-        return user
+        token_data = decode_access_token(token)
+        if token_data.type != "access":
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid Token type",
+                                headers={"WWW-Authenticate": "Bearer"})
+        user=db.query(user_model.User).filter(user_model.User.id == token_data.user_id, user_model.User.is_active==True).first()
+        if user is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="User not found",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        print(user)
+        return UserPublic.model_validate(user)
         
     except Exception as e:
         print(f"Exception in get_current_user: {type(e).__name__} - {str(e)}")
